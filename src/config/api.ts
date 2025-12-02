ff4b063d5837c161ce6c8928a73f24b064198ca2
Orlando Torres R <orlandoenriqueiii@gmail.com>
2025-12-02 18:14:48 -0400

    demo eliminado y api al mock

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://192.168.31.135:8080/api/v1';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api/v1';
 
 export const API_ENDPOINTS = {
   raffles: {
